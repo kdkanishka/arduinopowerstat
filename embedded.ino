a20a1f174f5e900fce13a10be48e2884d3da36c5
Kanishka <Kanishka Dilshan>
2015-03-06 19:50:27 +0530

    Measure voltage and amperage

--- embedded.ino
+++ embedded.ino
@@ -1,3 +1,6 @@
+// Arduino PIN definitions
+const int _VIN_PIN = A0;
+const int _CT_PIN = A1;
 //required data for Rpi
 float supplyVoltage = 0.0;
 float supplyAmp = 0.0;
@@ -7,7 +10,9 @@ float totalWattSeconds = 0.0;
 //data for internal calculations
 float _vcc = 0;
 
-//functions to calculate power info
+/**
+ *	calculates initial internal VCC and keep it for future reference
+ */
 long _readVcc() {
 	long result;
 	// Read 1.1V reference against AVcc
@@ -21,6 +26,57 @@ long _readVcc() {
   	return result;
 }
 
+/**
+ * Measure supply voltage
+ */
+void measureSupplyVoltage(){  
+	int samplingCount=512;
+	float adcResolution = 1023.0;
+	float zSq=0;
+	float halfVcc = _vcc / 2.0;
+	//ADC sampling
+	for(int i=0; i < samplingCount; i++){
+		int sensorValue = analogRead(_VIN_PIN);
+		float voltage = (sensorValue * _vcc) / adcResolution;
+		zSq+=pow(voltage-halfVcc,2.0);
+	}
+
+	float R1=100000.0; //100K in voltage divider
+	float R2=10000.0; //10K in voltage divider
+	float transformerFact = 21.8; //np/ns
+
+	float vRms = sqrt(zSq / samplingCount);
+	float vRealSec = vRms/(R2/(R1+R2));
+	float inputVoltage = vRealSec * transformerFact;
+	if(inputVoltage <= 1){
+		inputVoltage = 0;
+	}
+	Serial.print(inputVoltage);
+	Serial.println(" V");
+}
+
+/**
+ * Measure Amperage
+ */
+void measureSupplyAmperage(){
+	int samplingCount=512;
+	float adcResolution = 1023.0;
+	float zSq=0;
+	float halfVcc = _vcc / 2.0;
+	for(int i=0;i<samplingCount;i++){
+		int sensorValue = analogRead(_CT_PIN);
+		float ctmonVoltage = (sensorValue * _vcc) / adcResolution; // see text
+		zSq+=pow(ctmonVoltage-halfVcc,2.0);
+	}
+	//this vRms is propotional to the amperage
+	float ctMonFact = 30.0;
+	float vRms = sqrt(zSq / samplingCount);
+	float roundedvRms = round(vRms * 100)/100.0; //round value to two decimal points
+	float amps = roundedvRms * ctMonFact;
+	Serial.print(amps);Serial.println("A");
+}
+
+
 void setup()
 {
 	Serial.begin(9600);
@@ -30,5 +86,8 @@ void setup()
 
 void loop()
 {
-	
+	unsigned long calculateBegins = millis();
+	measureSupplyVoltage();
+	measureSupplyAmperage();
+	unsigned long calculationsEnds = millis();
 }
